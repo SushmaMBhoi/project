4a83e3b1d6da56167114a61b64d61123ea3f97fa
Shifa <Shifa@LAPTOP-0KJAA2VO>
2018-06-03 18:54:39 +0530

    first Commit.

--- MessagingSYSTEM/src/messagingsystem/MessagingSystem.java
+++ MessagingSYSTEM/src/messagingsystem/MessagingSystem.java
@@ -15,35 +15,39 @@ public class MessagingSystem {
         String name, password,userName;
         boolean valdationForLogin;
         SignUpSignIn ss = new SignUpSignIn();
-        int choice;
+        int choice=0;
         
         Scanner s=new Scanner(System.in);
-        
+            while(true){
             System.out.println("Please enter your choice\n");
-            System.out.println("1:Sign In\n");
-            System.out.println("2:Sign Up\n");
+            System.out.println("1:Sign In");
+            System.out.println("2:Sign Up");
+            System.out.println("3:Exit");
             choice=s.nextInt();
             
             switch(choice){
                 
-                case 1:System.out.println("Enter your Name\n");
+                case 1:System.out.println("Enter your Name");
                        name = s.next();
-                       System.out.println("Form a PASSWORD for your account that should start with a LETTER.\n");
+                       System.out.println("Form a PASSWORD for your account that should start with a LETTER.");
                        password = s.next();
                        userName = ss.signUp(name, password);
                        System.out.println("Your user name is:" + userName + "\n");
                        break;
                        
-                case 2:System.out.println("Enter User Name\n");
+                case 2:System.out.println("Enter User Name");
                        name = s.next();
-                       System.out.println("Enter Password\n");
+                       System.out.println("Enter Password");
                        password = s.next();
                        valdationForLogin = ss.signIn(name, password);
                        if(valdationForLogin==false)
                            System.out.println("Invald user name or password!");
                        break;
                        
+                case 3:System.exit(0);
+                       
                default:System.out.println("You entered an invalid choice\n");
+            }
             }
     }
     
