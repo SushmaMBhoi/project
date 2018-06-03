8551784d8587d81c51a56a4beb7d2856217db8f2
Shifa <Shifa@LAPTOP-0KJAA2VO>
2018-06-03 16:41:18 +0530

    first Commit.

--- MessagingSYSTEM/src/messagingsystem/MessagingSystem.java
+++ MessagingSYSTEM/src/messagingsystem/MessagingSystem.java
@@ -1,4 +1,5 @@
 package messagingsystem;
+import java.util.Scanner;
 
 /**
  *
@@ -10,7 +11,40 @@ public class MessagingSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        String name, password,userName;
+        boolean valdationForLogin;
+        SignUpSignIn ss = new SignUpSignIn();
+        int choice;
+        
+        Scanner s=new Scanner(System.in);
+        
+            System.out.println("Please enter your choice\n");
+            System.out.println("1:Sign In\n");
+            System.out.println("2:Sign Up\n");
+            choice=s.nextInt();
+            
+            switch(choice){
+                
+                case 1:System.out.println("Enter your Name\n");
+                       name = s.next();
+                       System.out.println("Form a PASSWORD for your account that should start with a LETTER.\n");
+                       password = s.next();
+                       userName = ss.signUp(name, password);
+                       System.out.println("Your user name is:" + userName + "\n");
+                       break;
+                       
+                case 2:System.out.println("Enter User Name\n");
+                       name = s.next();
+                       System.out.println("Enter Password\n");
+                       password = s.next();
+                       valdationForLogin = ss.signIn(name, password);
+                       if(valdationForLogin==false)
+                           System.out.println("Invald user name or password!");
+                       break;
+                       
+               default:System.out.println("You entered an invalid choice\n");
+            }
     }
     
 }
