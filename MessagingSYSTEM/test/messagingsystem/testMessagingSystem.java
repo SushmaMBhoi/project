a67ce1f55db226c26db158e2cc28682078eecede
Shifa <Shifa@LAPTOP-0KJAA2VO>
2018-06-08 20:01:13 +0530

    first Commit.

--- MessagingSYSTEM/test/messagingsystem/testMessagingSystem.java
+++ MessagingSYSTEM/test/messagingsystem/testMessagingSystem.java
@@ -1,20 +1,22 @@
 
 package messagingsystem;
 
+import java.util.ArrayList;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * A class to test methods of MessagingSystem class.
  * @author Shifa
  */
 public class testMessagingSystem {
-    SignUpSignIn s;
-    Operations o;
+    SignUpSignIn s,s1;
     public testMessagingSystem() {
     }
     
@@ -29,9 +31,10 @@ public class testMessagingSystem {
     @Before
     public void setUp() {
         s = new SignUpSignIn();
-        o = new Operations();
+        s1 = new SignUpSignIn();
+        
     }
-    
+        
     @After
     public void tearDown() {
     }
@@ -39,28 +42,30 @@ public class testMessagingSystem {
     @Test
     public void testSignUp() {
     
-        assertEquals("0Shifa",s.signUp("Shifa","shifa123"));
         
+        assertEquals("2Sara",s.signUp("Sara", "sara123"));
     }
     
     @Test
     public void testSignIn() {
-    
-        assertEquals(true,s.signIn("0Shifa","shifa123"));
+        
+        assertEquals(true,s1.signIn("0Shifa","shifa123"));
         
     }
     
     @Test
     public void testReadMails() {
-    
-        assertEquals(0,o.readMails());
+        assertEquals(true,s1.signIn("0Shifa","shifa123"));
+        assertEquals(0,s1.readMails());
         
     }
     
     @Test
     public void testWriteMail() {
-    
-        assertEquals(0,o.writeMail());
+        assertEquals("0Shifa",s1.signUp("Shifa","shifa123"));
+        assertEquals("1Sana",s.signUp("Sana","sana123"));
+        assertEquals(true,s1.signIn("0Shifa","shifa123"));
+        assertEquals(1,s1.writeMail("1Sana","Hello"));
         
     }
     
